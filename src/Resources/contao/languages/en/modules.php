f51f4f18a7ee2395cf97ae4c237fe559e4bc3de0
fritzmg <fmg@inspiredminds.at>
2018-06-14 15:33:30 +0200

    add english translation

---
+++ src/Resources/contao/languages/en/modules.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * TL_ROOT/vendor/srhinow/themecontent-bundle/src/Recources/contao/modules/languages/de/modules.php
+ *
+ * Contao extension: themecontent-bundle
+ * English translation file
+ *
+ * Copyright  Sven Rhinow Webentwicklung 2017 <http://www.sr-tag.de>
+ * Author     Sven Rhinow
+ * @license    LGPL-3.0-or-later
+ * Translator: Fritz Michael Gschwantner <https://github.com/fritzmg>
+ */
+
+/**
+ * Back end modules
+ */
+$GLOBALS['TL_LANG']['MOD']['theme_content'] = array('Theme content', '');
---
+++ src/Resources/contao/languages/en/tl_theme_section_article.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * TL_ROOT/vendor/srhinow/themecontent-bundle/src/Recources/contao/modules/languages/de/tl_theme_section_article.php
+ *
+ * Contao extension: themecontent-bundle
+ * English translation file
+ *
+ * Copyright  Sven Rhinow Webentwicklung 2017 <http://www.sr-tag.de>
+ * Author     Sven Rhinow
+ * @license    LGPL-3.0-or-later
+ * Translator: Fritz Michael Gschwantner <https://github.com/fritzmg>
+ */
+
+\System::loadLanguageFile('tl_article');
+$GLOBALS['TL_LANG']['tl_theme_section_article'] = $GLOBALS['TL_LANG']['tl_article'];
+$GLOBALS['TL_LANG']['tl_theme_section_article']['back_to_sectione_overview'] = "Back to sections";
